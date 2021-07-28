24b9f53674767793ae944da334e0b535f122940e
SSripilaipong <santhapon.s@siametrics.com>
2021-07-28 23:56:31 +0700

    add unit marker

--- tests/test_type_mapper.py
+++ tests/test_type_mapper.py
@@ -1,6 +1,8 @@
+import pytest
 from neomodel_constraints import Neo4jConstraintTypeMapper, UniqueConstraint
 
 
+@pytest.mark.unit
 def test_map_uniqueness_type():
     mapper = Neo4jConstraintTypeMapper()
     assert mapper.map('UNIQUENESS') == UniqueConstraint
