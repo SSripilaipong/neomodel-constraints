b8bee8c50ed30b102ab0aec70d008fa1d382861a
SSripilaipong <santhapon.s@siametrics.com>
2021-07-29 15:08:09 +0700

    add testcase for UniqueConstraint

--- tests/test_constraint_unique.py
+++ tests/test_constraint_unique.py
@@ -1,3 +1,4 @@
+import re
 import pytest
 
 from neomodel_constraints import ConstraintAbstract
@@ -127,3 +128,65 @@ def test_hash_dummy_with_same_data():
     c2 = DummyConstraint(['A', 'B'], ['x', 'y'])
 
     assert hash(c1) != hash(c2)
+
+
+@pytest.mark.unit
+def test_from_raw():
+    raw = {
+        'labels': ['A', 'B'],
+        'properties': ['x', 'y'],
+        'name': 'CCC',
+    }
+    constraint = UniqueConstraint.from_raw(raw)
+    assert all([
+        constraint.name == raw['name'],
+        constraint.properties == set(raw['properties']),
+        constraint.labels == set(raw['labels']),
+    ])
+
+
+@pytest.mark.unit
+def test_get_create_command_one_label():
+    u = UniqueConstraint(['A'], ['y'])
+    cmd = u.get_create_command()
+    assert cmd
+
+    expected_pattern = (r" *CREATE +CONSTRAINT +[^ ]+ +ON +\( *(?P<T1>[^ :]+) *:A *\)"
+                        r" +ASSERT +(?P<T2>[^ .]+).y *IS *UNIQUE *")
+    match = re.match(expected_pattern, cmd)
+    assert match
+
+    d = match.groupdict()
+    assert d['T1'] == d['T2']
+
+
+@pytest.mark.unit
+def test_get_create_command_two_labels():
+    u = UniqueConstraint(['A', 'Chain', 'C841N'], ['y'])
+    cmd = u.get_create_command()
+    assert cmd
+
+    expected_pattern = (r" *CREATE +CONSTRAINT +[^ ]+ +ON +\( *(?P<T1>[^ :]+) *(?P<L>(?:\:[^ :]+)+) *\)"
+                        r" +ASSERT +(?P<T2>[^ .]+).y *IS *UNIQUE *")
+    match = re.match(expected_pattern, cmd)
+    assert match
+
+    d = match.groupdict()
+    assert d['T1'] == d['T2']
+
+    ls = d['L']
+    assert ls[0] == ':' and set(ls.lstrip(':').split()) == {'A', 'Chain', 'C841N'}
+
+
+@pytest.mark.unit
+def test_get_drop_command():
+    u = UniqueConstraint(['A'], ['y'], name='my_constraint_1234')
+    cmd = u.get_create_command()
+    assert cmd
+
+    expected_pattern = r" *DROP +CONSTRAINT +(?P<NAME>[^ ]+) *"
+    match = re.match(expected_pattern, cmd)
+    assert match
+
+    d = match.groupdict()
+    assert d['NAME'] == 'my_constraint_1234'
