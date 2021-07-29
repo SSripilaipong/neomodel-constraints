c18521902b606403df53ff144ef4dc4b5f56d30a
SSripilaipong <santhapon.s@siametrics.com>
2021-07-29 14:43:11 +0700

    implement UniqueConstraint: _equals() and _get_data_hash()

--- neomodel_constraints/constraint/unique.py
+++ neomodel_constraints/constraint/unique.py
@@ -19,11 +19,16 @@ class UniqueConstraint(ConstraintAbstract):
         DROP CONSTRAINT {NAME}
         """
 
-    def _equals(self, other: 'ConstraintAbstract') -> bool:  # TODO
-        pass
+    def _equals(self, other: 'ConstraintAbstract') -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+        return self.labels == other.labels and self.properties == other.properties
 
-    def _get_data_hash(self) -> int:  # TODO
-        pass
+    def _get_data_hash(self) -> int:
+        labels_hash = sum(hash(e) for e in self.labels)
+        props_hash = sum(hash(e) for e in self.properties)
+
+        return labels_hash*2**3 + props_hash*3**3
 
     @classmethod  # TODO
     def from_raw(cls, data: Dict) -> 'ConstraintAbstract':
