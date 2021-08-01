b42563caf4ba82c8ee0f6aa4f1cb382b9c84f36d
SSripilaipong <santhapon.s@siametrics.com>
2021-08-02 01:34:41 +0700

    release 0.0.15

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 NAME = 'neomodel-constraints'
-VERSION = '0.0.10'
+VERSION = '0.0.15'
 URL = 'https://github.com/SSripilaipong/neomodel-constraints'
 LICENSE = 'MIT'
 AUTHOR = 'SSripilaipong'
