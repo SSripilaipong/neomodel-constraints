82dc6ed8b336d519c7523198490256edc728fe5e
SSripilaipong <santhapon.s@siametrics.com>
2021-08-05 02:26:20 +0700

    modify ConstraintsFetcher: remove redundant code

--- neomodel_constraints/fetcher/constraints/v4_2.py
+++ neomodel_constraints/fetcher/constraints/v4_2.py
@@ -5,6 +5,7 @@ from neomodel_constraints.constraint import ConstraintSet, TypeMapperAbstract
 from neomodel_constraints.fetcher.abstract import FetcherAbstract
 
 from .data import Neo4jConstraintQueryRecord
+from .util import convert_constraints_with_type_mapper
 
 
 class ConstraintsFetcherV4s2(FetcherAbstract):
@@ -17,12 +18,7 @@ class ConstraintsFetcherV4s2(FetcherAbstract):
         return [Neo4jConstraintQueryRecord(**record) for record in raw]
 
     def _convert_constraints(self, raw: List[Neo4jConstraintQueryRecord]) -> ConstraintSet:
-        constraints = set()
-        for record in raw:
-            constraint_type = self.type_mapper.map(record.type_)
-            constraint = constraint_type.from_raw(record.dict())
-            constraints.add(constraint)
-        return ConstraintSet(constraints)
+        return convert_constraints_with_type_mapper(raw, self.type_mapper)
 
     def fetch(self) -> ConstraintSet:
         raw: List[Neo4jConstraintQueryRecord] = self._fetch_raw_data()
